4077a45dee1cb56d73303ed771bc0885babbb839
YoungMay_ <102332172@qq.com>
2018-07-26 14:30:50 +0800

    调整数组顺序，使得所有奇数位于数组前半部分

--- test14.c
+++ test14.c
@@ -24,6 +24,34 @@ void ReorderOddEven(int* data, unsigned int length)
     }
 }
 
+//解法2-》推荐
+    //可复用，传入函数指针，控制调整条件
+int isEven(int n)//控制调整条件，可改变
+{
+    return (n&1)==0;
+}
+
+void ReorderOddEven1(int* data, unsigned int length, int (*func)(int))
+{
+    if(data==NULL || length==0)
+        return;
+    int* left = data;
+    int* right = data+length-1;
+    while(left < right)
+    {
+        while(left<right && !func(*left))
+            left++;
+        while(left<right && func(*right))
+            right--;
+        if(left < right)
+        {
+            int tmp = *left;
+            *left = *right;
+            *right = tmp;
+        }
+    }
+}
+
 void Print(int* data, int len)
 {
     int i = 0;
@@ -34,12 +62,27 @@ void Print(int* data, int len)
     printf("\n");
 }
 
-int main()
+void Test()
 {
     int data[] = {3, 2, 1, 4, 6, 9, 11, 14, 15};
     int size = sizeof(data)/sizeof(data[0]);
     Print(data, size);
     ReorderOddEven(data, size);
     Print(data, size);
+}
+
+void Test1()
+{
+    int data[] = {3, 2, 1, 4, 6, 9, 11, 14, 15};
+    int size = sizeof(data)/sizeof(data[0]);
+    Print(data, size);
+    ReorderOddEven1(data, size, isEven);
+    Print(data, size);
+}
+
+int main()
+{
+    Test();
+    Test1();
     return 0;
 }
