46d837314b5f8728c0f23e0c644edc14fdd9b54f
YoungMay_ <102332172@qq.com>
2018-07-27 21:15:30 +0800

    二叉树的镜像

---
+++ test19.cc
@@ -0,0 +1,24 @@
+//二叉树的镜像
+#include <iostream>
+using namespace std;
+
+struct BinaryTreeNode
+{
+    int val;
+    BinaryTreeNode* left;
+    BinaryTreeNode* right;
+};
+
+void TreeInit(BinaryTreeNode** root)
+{
+    if(root == NULL)
+        return;
+    *root = NULL;
+    return;
+}
+
+int main()
+{
+
+    return 0;
+}
